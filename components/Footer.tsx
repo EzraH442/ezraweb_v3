7c0ccf8f0b31a01e426c14d65d097fe190ceb90f
Ezra Huang <ezrahuang155@gmail.com>
2023-11-29 16:52:11 -0500

    update packages

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,19 @@
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import React from "react";
 
 const Footer: React.FC<{}> = () => (
   <footer className="text-white h-24 flex flex-col justify-center items-center">
     <span className="inline-flex justify-center space-x-2">
-      <a href="mailto:ezrahuang155@gmail.com">
+      <a href="mailto:ezrahuang155@gmail.com" aria-label="Email">
         <FontAwesomeIcon icon={faEnvelope} size="1x" width={10} />
       </a>
-      <a href="https://www.linkedin.com/in/ezrahuang">
+      <a href="https://www.linkedin.com/in/ezrahuang" aria-label="Linkedin">
         <FontAwesomeIcon icon={faLinkedin} size="1x" width={10} />
       </a>
-      <a href="https://github.com/EzraH442">
+      <a href="https://github.com/EzraH442" aria-label="GitHub">
         <FontAwesomeIcon icon={faGithub} size="1x" width={10} />
       </a>
     </span>
