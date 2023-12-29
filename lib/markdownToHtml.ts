3e54b5da37f2c4d2dec451ac08e4fd90500323ed
Ezra Huang <ezrahuang155@gmail.com>
2023-12-28 22:55:34 -0700

    update markdown parsing method and new post

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,12 +1,22 @@
-import { remark } from "remark";
-import html from "remark-html";
-import { VFileCompatible } from "vfile";
-import prism from "remark-prism";
+import remarkGfm from "remark-gfm";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import { unified } from "unified";
+import rehypeStringify from "rehype-stringify";
+import rehypeFormat from "rehype-format";
+import remarkPrism from "remark-prism";
 
-export default async function markdownToHtml(markdown: VFileCompatible) {
-  const result = await remark()
-    .use(html, { sanitize: false })
-    .use(prism, { transformInlineCode: true })
+export default async function markdownToHtml(markdown: string) {
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkFrontmatter)
+    .use(remarkGfm)
+    // @ts-ignore
+    .use(remarkPrism, { transformInlineCode: true })
+    .use(remarkRehype)
+    .use(rehypeFormat)
+    .use(rehypeStringify)
     .process(markdown);
   return result.toString();
 }
